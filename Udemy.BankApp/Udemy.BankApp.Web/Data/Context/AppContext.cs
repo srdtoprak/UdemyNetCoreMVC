32ee6ee93807e8cf17276f5756be48abae36e356
srdtoprak <85314700+srdtoprak@users.noreply.github.com>
2021-06-03 23:01:57 +0300

    core mvc test
    test

---
+++ Udemy.BankApp/Udemy.BankApp.Web/Data/Context/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Udemy.BankApp.Web.Data.Context
+{
+    public class AppContext
+    {
+    }
+}
